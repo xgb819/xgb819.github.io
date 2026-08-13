a5807dfe29ff98c076b94af20838e37792f6a286
xgb819 <xgb819@gmail.com>
2026-08-13 11:17:27 +0800

    feat: update equipment showcase

--- src/config/equipment.ts
+++ src/config/equipment.ts
@@ -4,10 +4,22 @@ export const equipment = [
 
         items: [
             {
-                name: "宏碁 暗影骑士 5 AN515-57",
+                name: "宏碁暗影骑士・擎 5",
                 specification: "i5-11400H / RTX 3050 Laptop",
                 image: "/images/equipment/acer-nitro-5-an515-57.png",
+                url: "https://www.acer.com.cn/product_list.html?parentid=336"
+            },
+            {
+                name: "创维 X24PF18 电竞显示器",
+                specification: "23.8 英寸 / 200Hz",
+                image: "/images/equipment/skyworth-x24pf18.png",
                 url: ""
+            },
+            {
+                name: "雷蛇 × 宝可梦外设全家桶",
+                specification: "耿鬼款",
+                image: "/images/equipment/razer-pokemon-gengar.png",
+                url: "https://cn.razerzone.com/collabs/pokemon-gengar"
             },
             {
                 name: "真我 Neo 8",
@@ -26,6 +38,36 @@ export const equipment = [
                 specification: "流光紫",
                 image: "/images/equipment/huawei-freeclip.png",
                 url: "https://consumer.huawei.com/cn/headphones/freeclip/"
+            },
+            {
+                name: "小米手环 8",
+                specification: "黑色",
+                image: "/images/equipment/xiaomi-smart-band-8.png",
+                url: "https://www.mi.com/xiaomi-shouhuan-8"
+            },
+            {
+                name: "雷蛇骑仕 V2 手游手柄",
+                specification: "USB Type C",
+                image: "/images/equipment/razer-kishi-v2.png",
+                url: "https://cn.razerzone.com/mobile-controllers/razer-kishi-v2"
+            }
+        ]
+    },
+    {
+        category: "生活日用",
+
+        items: [
+            {
+                name: "象印不锈钢真空保温杯",
+                specification: "丁香紫 / 480mL",
+                image: "/images/equipment/zojirushi-sm-zb48.png",
+                url: "https://www.zojirushi-china.com/product/glass/seamless/sm-zb/"
+            },
+            {
+                name: "米家声波扫振电动牙刷",
+                specification: "白色",
+                image: "/images/equipment/xiaomi-electric-toothbrush.png",
+                url: "https://www.mi.com/shop/buy/detail?product_id=20448"
             }
         ]
     }
